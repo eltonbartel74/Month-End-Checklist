a1fba71f4d3ef4da9b47afae8b82c1a248dfee65
eltonbartel74 <eltonbartel@icloud.com>
2026-03-08 22:10:54 +1030

    UX: slightly shrink Task column and expand Comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -971,14 +971,14 @@ export default function Home() {
             <colgroup>
               <col style={{ width: "3%" }} />
               <col style={{ width: "10%" }} />
-              <col style={{ width: "27%" }} />
+              <col style={{ width: "26%" }} />
               <col style={{ width: "10%" }} />
               <col style={{ width: "6%" }} />
               <col style={{ width: "5%" }} />
               <col style={{ width: "16%" }} />
               <col style={{ width: "9%" }} />
               <col style={{ width: "7%" }} />
-              <col style={{ width: "7%" }} />
+              <col style={{ width: "8%" }} />
             </colgroup>
             <thead>
               <tr className="border-b border-white/10 text-xs text-white/70">
