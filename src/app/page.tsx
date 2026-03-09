d8d5386bb8e14c2d6eb4aac5fe1d79ccf8df3a91
eltonbartel74 <eltonbartel@icloud.com>
2026-03-09 16:13:49 +1030

    Widen Promised date/ETA columns so date fields fit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1100,13 +1100,13 @@ export default function Home() {
             <colgroup>
               <col style={{ width: "3%" }} />
               <col style={{ width: "10%" }} />
-              <col style={{ width: "21%" }} />
+              <col style={{ width: "20%" }} />
               <col style={{ width: "10%" }} />
               <col style={{ width: "6%" }} />
               <col style={{ width: "5%" }} />
-              <col style={{ width: "16%" }} />
-              <col style={{ width: "9%" }} />
-              <col style={{ width: "7%" }} />
+              <col style={{ width: "15%" }} />
+              <col style={{ width: "10%" }} />
+              <col style={{ width: "8%" }} />
               <col style={{ width: "13%" }} />
             </colgroup>
             <thead>
